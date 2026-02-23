8e7eaa8022c3e3a33eb90839d1f57e9e3bcca886
Antoine <aa.augsburger@gmail.com>
2026-02-23 11:37:09 +0100

    struct

---
+++ Client.py
@@ -0,0 +1,31 @@
+import socket
+
+#adresse et port du serveur
+server_adress = "vlbelintrocrypto.hevs.ch"
+server_port = 6000
+
+class Client():
+    def __init__(self):
+        self.connect()
+
+    def connect(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((server_adress, server_port))
+
+
+    def send(self, text):
+        data = text.encode("utf-8")
+        self.sock.send(data)
+
+#recevoir un message
+    # def receive(self):
+    #
+    #
+
+    #ferner la connection
+    # def close(self):
+    #
+
+
+
+
---
+++ MessageHandler.py
@@ -0,0 +1,44 @@
+class MessageHandler:
+    def __init__(self):
+
+
+    #Convertir entier en octet
+    def int_to_bytes:
+
+
+    #Convert octet en int
+    def bytes_to_int:
+
+    #convertir texte en liste de nombre
+    def string_to_ints:
+
+    #convertir liste de nombre en text utf-8
+    def ints_to_string:
+
+    #stard big endians
+    def encode_ints:
+
+    def decode_ints:
+
+
+    #creer un message au format protocle ISC
+    def create_text_message(text, bytes_per_char, is_server=False):
+
+    def create_image_message(width, height, image_data):
+
+    #réception séquentielle :
+
+    #ajouter les nouvelles données recu dans un buffer
+    def add_data(self, data):
+
+    #parcourir le buffer et extraire les messages bruts
+    def get_messages(self):
+
+    #extraction final
+
+    #prendre un message ISC brut et extraire le text en clair
+    def parse_text_message(message, bytes_per_char):
+
+
+    #extraire les données des pixels RGB d'un message ISC avec image dedans
+    def parse_image_message(message):
