9219c3d0f18c0227e9a9af455b58d570f69d8ceb
retilol <zian2002@outlook.com>
2026-03-16 11:26:27 +0100

    start working on seganography

--- Crypto/Shift.py
+++ Crypto/Shift.py
@@ -20,13 +20,3 @@ def decode_shift_int(int_list):
         shifted_list = unshift_int(int_list, s)
         output.append(shifted_list)
     return output
-
-
-"""
-shift_key = 5
-message = "bonjour"
-shifted_message=shift(message,shift_key)
-print(shifted_message)
-unshifted_message =unshift(shifted_message,shift_key)
-print(unshifted_message)
-"""
