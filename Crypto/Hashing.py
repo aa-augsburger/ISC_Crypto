4c26e0130b9d5d140030dc4473a7c279dfe8a1df
retilol <zian2002@outlook.com>
2026-03-30 09:52:08 +0200

    hashing implementation

--- Crypto/Hashing.py
+++ Crypto/Hashing.py
@@ -1,2 +1,15 @@
 import secrets
+import os
+import hashlib
 
+
+
+def hash(password):
+    salt = os.urandom(16)
+    combined = salt + password.encode()
+    hashed = hashlib.sha256(combined).hexdigest()
+    return hashed
+
+
+key = "helo"
+print(hash(key))
