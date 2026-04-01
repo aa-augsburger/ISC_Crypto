36920fabf97a993f42b947169e353ce0913bce85
retilol <zian2002@outlook.com>
2026-04-01 14:44:18 +0200

    Update Hashing.py

--- Crypto/Hashing.py
+++ Crypto/Hashing.py
@@ -3,13 +3,13 @@ import os
 import hashlib
 
 
-
-def hash(password):
+def hash(password):#enter a texte, it will be hashed into a 256bits word
     salt = os.urandom(16)
     combined = salt + password.encode()
     hashed = hashlib.sha256(combined).hexdigest()
     return hashed
 
-
+"""
 key = "helo"
 print(hash(key))
+"""
